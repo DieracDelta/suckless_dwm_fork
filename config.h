d88837b4771c1e75d31102283f28dd74d2470f6e
Justin Restivo <jrestivo@mit.edu>
2018-01-04 00:01:39 -0500

    up and working fonts

--- config.h
+++ config.h
@@ -2,7 +2,8 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=10"
+  "FontAwesome:size=8",
+  "Inconsolata:size=8",
 };
 static const char dmenufont[]       = "monospace:size=10";
 static const char normbordercolor[] = "#444444";
@@ -17,7 +18,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"", "", "", "", "", "",  "", "", ""};
 
 #define NUMCOLORS         4
 static const char colors[NUMCOLORS][MAXCOLORS][8] = {
